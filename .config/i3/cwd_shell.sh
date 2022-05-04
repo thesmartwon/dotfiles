bd33cf91aeee9b5da57eb5ac1bef1f74a8c6ceb8
zack <zack@polygon.io>
2022-05-04 12:17:55 -0400

    work settings

--- .config/i3/cwd_shell.sh
+++ .config/i3/cwd_shell.sh
@@ -3,7 +3,7 @@ ID=$(xdpyinfo | grep "focus" | sed -nr 's#.*(0x[[:xdigit:]]+).*#\1#p')
 PID=$(xprop -id $ID | grep -m 1 PID | cut -d " " -f 3)
 CHILD_PID=$(pgrep -P $PID)
 if [ -e "/proc/$CHILD_PID/cwd" ]; then
-i3-sensible-terminal --working-directory $(readlink /proc/$CHILD_PID/cwd)
+  i3-sensible-terminal --working-directory $(readlink /proc/$CHILD_PID/cwd)
 else
-i3-sensible-terminal --working-directory ~/src
+  i3-sensible-terminal --working-directory ~/src/github.com/polygon-io
 fi
